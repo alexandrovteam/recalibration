c40e2f8241f0773415fae9daed7ea4f38bff319d
andy-d-palmer <palmer@embl.de>
2018-04-16 11:14:56 +0200

    make ppm a flag optional fit plot actually use min/max mz

--- recalibration/recalibration.py
+++ recalibration/recalibration.py
@@ -27,20 +27,25 @@ def lsfunc(x, t, y):
     return e
 
 def fit_spectrum(mzs, intensities, ref_mzs, ref_pcts, max_delta_ppm, mz_min, mz_max, x0=(1, 1, 1),
-                 weight_by_occurance=True, stabilise=True, intensity_threshold=0):
-
+                 weight_by_occurance=True, stabilise=True, intensity_threshold=0, ppm_flag=True):
     mzs, intensities = map(lambda x: x[intensities>intensity_threshold], [mzs, intensities])
+    mzs, intensities = map(lambda x: x[mzs > mz_min], [intensities, mzs])
+    mzs, intensities = map(lambda x: x[mzs < mz_max], [intensities, mzs])
+    ref_mzs, ref_pcts = map(lambda x: x[ref_mzs > mz_min], [ref_pcts, ref_mzs])
+    ref_mzs, ref_pcts = map(lambda x: x[ref_mzs < mz_max], [ref_pcts, ref_mzs])
 
-    delta = -utils.get_deltas_mix(mzs, ref_mzs,  ppm=True)
+    delta = -utils.get_deltas_mix((mzs, intensities), (ref_mzs, ref_pcts),  ppm=ppm_flag)
     delta[np.abs(delta) > max_delta_ppm] = np.nan
 
     ref_mzs, ref_pcts, delta = map(lambda x:x[~np.isnan(delta)], [ref_mzs, ref_pcts, delta])
-    if stabilise:
-        _x = [mz_min, mz_max]
-        _y = [0, 0]
-    else:
-        _x = []
-        _y = []
+
+    _x = []
+    _y = []
+    if stabilise is not None:
+        for s in range(stabilise):
+            _x.extend([mz_min,mz_max])
+            _y.extend([0, 0])
+
 
     for ref_mz, ref_pct, dt in zip(ref_mzs, ref_pcts.astype('int'), delta):
         if not weight_by_occurance:
@@ -52,15 +57,26 @@ def fit_spectrum(mzs, intensities, ref_mzs, ref_pcts, max_delta_ppm, mz_min, mz_
     _r = least_squares(lsfunc, x0, loss='soft_l1', f_scale=0.5, args=(_x, _y))
     return _r, (_x, _y)
 
-def adjust_spectrum(mzs, ints, ref_mzs, max_delta_ppm=50, x0=[0,0,0]):
+def get_shift_model(mzs, ints, ref_mzs, max_delta_ppm=30, x0=(0,0,0), stabilise=1, ppm_flag=True, plot=False):
     r, data = fit_spectrum(
-        mzs, ints,
+        *utils.select_peaks(mzs, ints, max_per_chunk=1, bins=np.arange(mzs[0], mzs[-1], 0.1)),
+        #mzs, ints,
         ref_mzs['mz'].values, ref_mzs['count'],
         mz_min=mzs.min(), mz_max=mzs.max(),
-        max_delta_ppm=max_delta_ppm, x0=x0, stabilise=True
+        max_delta_ppm=max_delta_ppm, x0=x0, stabilise=stabilise, ppm_flag=ppm_flag
     )
-    shifts = poly(r.x, mzs)
-    return  mzs - shifts*1e-6*mzs
+    if plot:
+        import matplotlib.pyplot as plt
+        shifts = poly(r.x, mzs)
+        plt.figure()
+        plt.scatter(data[0], data[1], label='exp')
+        plt.plot(mzs, shifts, label='fit')
+        plt.legend()
+        plt.show()
+    if ppm_flag:
+        return lambda x: poly(r.x, x)*1e-6*x
+    else:
+        return lambda x: poly(r.x, x)
 
 def mean_spectrum_aligned(imzml):
     mzs, _ = map(np.asarray, imzml.getspectrum(0))
@@ -71,14 +87,18 @@ def mean_spectrum_aligned(imzml):
     return mzs, intensities / float(ii)
 
 
-def recalibrate(imzml_fn, imzml_out_fn, ref_mzs):
-    imzml = ImzMLParser.ImzMLParser(imzml_fn)
-    ms_mzs, ms_ints = mean_spectrum_aligned(imzml)
-    ms_mzs = adjust_spectrum(ms_mzs, ms_ints, ref_mzs)
+def recalibrate(aligned_fn, clustered_fn, imzml_out_fn, ref_mzs, max_delta=30, x0 = (0,0,0), stabilise=1, ppm_flag=True, plot=False):
+    aligned_imzml = ImzMLParser.ImzMLParser(aligned_fn)
+    clustered_imzml = ImzMLParser.ImzMLParser(clustered_fn)
+    ms_mzs, ms_ints = mean_spectrum_aligned(clustered_imzml)
+    shift_model = get_shift_model(ms_mzs, ms_ints, ref_mzs, x0 = x0,
+                             ppm_flag=ppm_flag, max_delta_ppm=max_delta,
+                             plot=plot, stabilise=stabilise)
     with ImzMLWriter.ImzMLWriter(imzml_out_fn) as imzml_out:
-        for ii, c in enumerate(imzml.coordinates):
-            mzs, counts = map(np.asarray, imzml.getspectrum(ii))
-            imzml_out.addSpectrum(ms_mzs, counts, coords=(c[0], c[1], 1))
+        for ii, c in enumerate(aligned_imzml.coordinates):
+            mzs, counts = map(np.asarray, aligned_imzml.getspectrum(ii))
+            mzs = mzs+shift_model(mzs)
+            imzml_out.addSpectrum(mzs, counts, coords=(c[0], c[1], 1))
 
 
 def _get_neighbours(graph, node):
@@ -103,25 +123,29 @@ def calculate_node_offset(graph):
     return graph
 
 
-def adjust_graph(graph, reps=500):
+def adjust_graph(graph, reps=750):
         for node in graph.nodes:
-            graph.nodes[node]['offset'] = 0.
+            graph.nodes[node].setdefault('offset', 0.)
         for ii in range(reps):
             graph = calculate_node_offset(graph)
         return graph
 
 
-def write_tmp_imzml(graph, imzml_fn, imzml_out_fn ):
+def write_tmp_imzml(graph, imzml_fn, imzml_out_fn, ppm=True ):
     imzml = ImzMLParser.ImzMLParser(imzml_fn)
     with ImzMLWriter.ImzMLWriter(imzml_out_fn) as imzml_out:
         for ii, c in enumerate(imzml.coordinates):
             mzs, counts = map(np.asarray, imzml.getspectrum(ii))
-            mzs = mzs - 1e-6*mzs*graph.nodes[str(ii)]['offset']
+            if ppm:
+                delta_mz = 1e-6*mzs*float(graph.nodes[str(ii)]['offset'])
+            else:
+                delta_mz = float(graph.nodes[str(ii)]['offset'])
+            mzs = mzs - delta_mz
             imzml_out.addSpectrum(mzs, counts, coords=(c[0], c[1], 1))
 
 
-def align(imzml_fn, tmp_fn):
-    graph = shift_graph.create_mass_shift_graph(imzml_fn=imzml_fn, num_cores = 6)
+def align(imzml_fn, tmp_fn, ppm=True):
+    graph = shift_graph.create_mass_shift_graph(imzml_fn=imzml_fn, num_cores = 6, ppm=ppm)
     graph = adjust_graph(graph)
     write_tmp_imzml(graph, imzml_fn, tmp_fn)
     return graph
@@ -136,22 +160,34 @@ def cluster(imzml_fn, imzml_out_fn, imzb_fn = "./tmp.imzb", cluster_ppm = 0.2):
     bins = np.sort(np.concatenate([dbscan.left, dbscan.right]))
     # Rebin and write
     centroid_mzs = dbscan['mean']
-    print(len(centroid_mzs))
     imzml = ImzMLParser.ImzMLParser(imzml_fn)
     with ImzMLWriter.ImzMLWriter(imzml_out_fn) as imzml_out:
         for ii, c in enumerate(imzml.coordinates):
             mzs, counts = map(np.asarray, imzml.getspectrum(ii))
             counts = np.bincount(np.digitize(mzs, bins), weights=counts, minlength=len(bins))
             imzml_out.addSpectrum(centroid_mzs, counts[1::2], coords=(c[0], c[1], 1))
+    print(ii)
 
-def pipeline(imzml_fn, imzml_recal_fn, config, metadata, tmp_fn = "tmp.imzML"):
+def pipeline(imzml_fn, imzml_recal_fn, config, metadata, tmp_fn = "tmp.imzML", ppm_flag=True, align_flag=True):
 #    config = json.load(open('../metaspace_es_config.json'))
-    align(imzml_fn, tmp_fn)
+    if align_flag:
+        align(imzml_fn, tmp_fn, ppm_flag)
+    else:
+        # spoof the alignment
+        subprocess.check_call(["cp", imzml_fn, tmp_fn])
+        subprocess.check_call(["cp", imzml_fn.replace('.imzML','.ibd'), tmp_fn.replace('.imzML','.ibd')])
+
     cluster(tmp_fn, tmp_fn.replace(".imzML", "_cl.imzML"), tmp_fn.replace(".imzML", ".imzb"))
     ref_mzs = pd.concat(
             get_reference_mzs(polarity=metadata['polarity'], tissue_type=metadata['organ'],
                               source=metadata['source'], database=metadata['database'],
-                              fdr = 0.1, config=config)
+                              fdr = 0.1, config=config, min_count=5)
         , axis=1)
-    print(ref_mzs.shape)
-    recalibrate(tmp_fn.replace(".imzML", "_cl.imzML"), imzml_recal_fn, ref_mzs)
+    align_fn = tmp_fn
+    cluster_fn = tmp_fn.replace(".imzML", "_cl.imzML")
+    recalibrate(align_fn, cluster_fn, imzml_recal_fn, ref_mzs,
+                          ppm_flag=True,
+                          max_delta=30,
+                          x0=(0,0,0),
+                          stabilise=1,
+                          )

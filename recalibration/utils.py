b8dee62e8e7e1cafa4acb1a71dd16c7e8b5ad31f
andy-d-palmer <palmer@embl.de>
2018-05-14 10:37:00 +0200

    refactoring added plotting

--- recalibration/utils.py
+++ recalibration/utils.py
@@ -31,7 +31,20 @@ def find_nearest(v, t):
     ix = ix-1+np.argmin(np.abs(np.vstack([v[ix-1], v[ix]]) - t[::]), axis=0)
     return ix, v[ix]
 
-def get_deltas_mix(target, reference, ppm=True):
+def get_matched_peaks(target, reference):
+    dmz = np.abs(target[:, 0][:, np.newaxis] - reference[:, 0])
+    ix0 = np.arange(dmz.shape[0])
+    ix1 = np.argmin(dmz, axis=1)
+    ix2 = np.arange(dmz.shape[1])
+    ix3 = np.argmin(dmz, axis=0)
+    assert len(ix0) == len(ix1)
+    assert len(ix2) == len(ix3)
+    matchix = set((x, y) for x, y in zip(ix2, ix3)) & set((x, y) for x, y in zip(ix1, ix0))
+    m1 = np.asarray([target[m[1], 0] for m in matchix])
+    m2 = np.asarray([reference[m[0], 0] for m in matchix])
+    return m1, m2, matchix
+
+def get_deltas_mix(target, reference, ppm=True, plot=False):
     """
     matches most likely pairs of peaks between two vectors and returns a list of value differences
     :param target: vector one
@@ -39,21 +52,24 @@ def get_deltas_mix(target, reference, ppm=True):
     :return: list of pairwise differences, sorted in ascending value order. length == len(v2), nan where matching value not found
     """
     target, reference = map(lambda x: np.asarray(x).T, [target, reference])
-    dmz = np.abs(target[:, 0][:, np.newaxis] - reference[:,0])
-    ix0 = np.arange(dmz.shape[0])
-    ix1 = np.argmin(dmz, axis=1)
-    assert len(ix0) == len(ix1)
-    ix2 = np.arange(dmz.shape[1])
-    ix3 = np.argmin(dmz, axis=0)
-    assert len(ix2) == len(ix3)
-    matchix = set((x,y) for x,y in zip(ix2,ix3)) & set((x,y) for x,y in zip(ix1, ix0))
-    m1 = np.asarray([target[m[1],0] for m in matchix])
-    m2 = np.asarray([reference[m[0],0] for m in matchix])
+    m1, m2, matchix = get_matched_peaks(target, reference)
     _deltas = m1 - m2
     if ppm:
-        _deltas = 1e6*_deltas/np.mean(np.vstack([m2, m2]), axis=0)
+        _deltas = 1e6*_deltas/m2
     deltas = np.nan * np.zeros(reference.shape[0])
     deltas[[m[0] for m in matchix]] = _deltas
+    if plot:
+        import matplotlib.pyplot as plt
+        def stemplot(x, y, offset=0):
+            plt.errorbar(x, y + offset, yerr=(y, np.zeros(len(x))), fmt=".", markersize=0)
+        plt.figure()
+        stemplot(target[:,0], np.log(target[:,1]), offset=0.2)
+        stemplot(reference[:,0], -np.log(reference[:,1]), offset=-.2)
+        for _m1, _m2 in zip(m1, m2):
+            plt.plot([_m1, _m2], [0.2,-0.2], "k-")
+        plt.xlabel('m/z')
+        plt.ylabel('intensity ln()')
+        plt.show()
     return deltas
 
 
@@ -130,12 +146,28 @@ def select_peaks(mzs, intensities, max_per_chunk=75, n_chunks=10, bins=None):
     return mzs[ixs], intensities[ixs]
 
 
-def estimate_linear_shift(s1, s2, n=None, ppm=True, n_chunks=1):
-    bins = np.linspace(np.min([s1[0][0], s2[0][0]]), np.max([s1[0][-1], s2[0][-1]]), n_chunks+1)
-    #v1, v2 = [select_peaks(*s, bins=bins, max_per_chunk=n) for s in [s1, s2]]
-    v1, v2 = [get_top_peaks(*s, n) for s in [s1, s2]]
-    deltas = get_deltas_mix(v2, v1, ppm=ppm)
+def estimate_linear_shift(s1, s2, n=None, ppm=True, n_chunks=1, plot=False, f_scale=0.05):
+    from scipy.optimize import minimize, least_squares
+    bins = np.arange(np.min([s1[0][0], s2[0][0]]), np.max([s1[0][-1], s2[0][-1]])+0.1, 0.2)
+    v1, v2 = [select_peaks(*s, bins=bins, max_per_chunk=1) for s in [s1, s2]]
+    v1, v2 = [get_top_peaks(*s, n) for s in [v1, v2]]
+    deltas = get_deltas_mix(v2, v1, ppm=ppm, plot=plot)
     deltas = np.sort(deltas[~np.isnan(deltas)])
     if len(deltas) == 0:
         return 0
-    return -np.percentile(deltas, [25, 75])[np.argmax([0, np.median(deltas)])]
+    #basefunc = lambda x, t: x[0]*np.tan( x[2]*(np.pi / len(t)) * (t + (len(t)+1) / 2.)) + x[1]
+    #basefunc = lambda x,t: np.polyval(x, t)
+    #lsfunc = lambda x,t,y: basefunc(x, t) - y
+    #x0 = (0,0,0,0,0)
+    #_r = least_squares(lsfunc, x0, loss='soft_l1', f_scale=f_scale, args=(np.arange(len(deltas)), deltas))
+    if plot:
+        import matplotlib.pyplot as plt
+        plt.figure()
+        plt.plot(np.sort(deltas), "k")
+        #_r = least_squares(lsfunc, x0, loss='soft_l1', f_scale=f_scale, args=(np.arange(len(deltas)), deltas))
+        #plt.plot(basefunc(_r.x, np.arange(len(deltas))))
+        #print(_r.x, -basefunc(_r.x, np.arange(len(deltas)))[int(len(deltas) / 2.)])
+        plt.plot([0, len(deltas)], np.ones(2)*np.median(deltas), "g-")
+        print(np.median(deltas))
+        plt.show()
+    return -np.median(deltas) #-np.polyval(_r.x, len(deltas)/2)
